c246d3a7391828f2a2a2f64bec7820be7bebea76
MarinaStoneQA <marinastoneqa@gmail.com>
2021-01-17 15:38:06 +0200

    no message

---
+++ Lesson 23/prectice.js/practice.js
@@ -0,0 +1,190 @@
+
+// let i = 1;
+// while (i <=15) {
+//     console.log(' Ученик, i');
+//     i++;
+// }
+
+// let i = +prompt("Введи число: ") 
+// let s = ""  
+// while (i > 0) {     s = s + "#";     i--; } 
+// console.log(s);
+
+
+
+//введите число а на экран вывелись все числа от введенного до 0
+// let i = prompt('Введите число')
+// while( i > 0) {
+//     console.log( i )
+// }
+
+
+
+//Запросить число и степень Возведите число в указаную степень
+// let i = prompt('Введите число степень');
+// let power = prompt('Введите степень');
+// let res = 1;
+// while (power > 0) {
+//     res * = i;
+//     power--;
+// }
+// console.log(res);
+
+
+//  запросить два числа и натйти все общие делители
+
+// let i = +prompt('Введите число');
+// let j = +prompt('Введите число 2');
+// let a = i;
+// if ( i < j ){
+//     i = j,j = a;
+// }
+// let count = j;
+//  while(count >0 ){
+//      if (i % count== 0 && j % count==o){
+//          console.log(count);
+//      };
+//      count--;
+//  }
+
+
+
+// Посчитать факториал введеного пользователем числа  
+
+// let i = +rompt('');
+// let res = 1;
+
+// let count = j;
+
+// while ( i>0 ){
+//     res *=i;
+//     i--;
+// }
+// console.log(res)
+
+
+
+ //Предлагать пользователю решить пример 2 + 2 * 2  до тех пор пока пользователь не решит eё
+
+//  let i;
+//  do {
+//      let i  = +prompt ('Htibnt ghbvth 2 + 2 * 2')
+//  } while (i != 6 );
+
+
+
+ //Делить число 1000 на 2 да тех пор пока не получится число меньше 50
+
+//   let i = 1000;
+//   let count = 0;
+
+//   do {
+//       i / 2;
+//       count++
+//   } while(i > 50);
+//   console.log(i, count)
+  
+
+
+// let num = +prompt('Введите целое число') ;
+// for (let i = 1; i < 100 ; i++){
+//     if (i% num ==0){
+//         console.log('i');
+//     }
+// }
+
+
+// Ввести каждый 4ый элемент из указанного пользователем диапазона
+// Пользователь указыввет минимальное и максимальное значение диапазона
+
+// let num = +prompt('Введите целое число');
+// let num2 =+prompt('Введите целое число');
+
+// let min = 0;
+// let max = 0;
+
+// if (num > num2){
+//      max = num, min = num2;
+// } else { 
+//     max = num2, min = num;
+// }
+
+// for (let i = min; i <= max; i +=4 ){
+// console.log(i);
+// }
+
+ //Запрсить число и проверить простое ли оно. Простое число делиться без остатка токо на себя и еденицу
+
+// let num = +prompt('Введите целое число');
+// let count = 0;
+
+//  for (let i = 2 ; i < num && count < 4 ; i ++ ) {
+//         if (num % i == 0) {
+//             count++ 
+//         }
+//     }
+
+//     if (count == 2 ) { 
+//         console.log('Число нпростое')
+//     } else {
+//          console.log('Число не простое')
+//     }
+
+
+
+//24 урок функции:
+
+
+// let result = getMax (99, 165);
+
+// alert( result );
+
+
+// function getMax (a, b) {
+//     if (a > b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+//   }
+
+ //Написать ф кот выводит переданое число в указаную степень
+
+
+// function getPower (num, power) {
+//         return Math.pow(num, power);
+//     }
+
+// let num = +prompt ('Введите число');
+
+// let result = getPower(num, 2);
+
+// console.log(result);
+
+
+ //Написать ф которая принимает 2 числа и знак () + - ; /) 
+   
+ let num1 = +prompt(' Введите число 1'),
+     num2 = +prompt(' Введите число 2'),
+     symb = prompt(' Введите действие  + - ; /'); 
+
+function getCalc(num1, num2, symb = '+') {
+        if (symb =='+'){
+    return num1 + num2;
+    }
+    if (symb =='-'){
+        return num1 - num2;
+    }
+    if (symb =='*'){
+        return num1 * num2;
+    }
+    if (symb =='/'){
+        return num1 / num2;
+    }
+    if (symb ==''){
+        return num1 + num2;
+    }
+    }
+ let result = getCalc(num1, num2, symb);
+
+ console.log(result);
---
+++ lesson20/practice.js
@@ -0,0 +1,176 @@
+
+/* let num = Number(prompt("Number?"));
+let num2 = +prompt("Number 2?");
+let ev = (num+num2)/2;
+
+console.log(ev);*/
+
+/*let num = Number(prompt("Number?"));
+let num2 = +prompt("Number 2?");
+
+let plus=  num + num2;
+let min = num - num2;
+let multiplay = num * num2;
+let div = num / num2:
+
+console.log('+',plus ,'-',min,'*',multiplay,'/',div);*/
+
+
+/* let num = +prompt("Number?");
+let num2 = +prompt("Number 2?");
+let x = -(num2/num);
+console.log('x=',x) */
+
+/*
+const HOUERS = 23;
+const MIN = 60;
+
+let num = +prompt("Number?");
+let num2 = +prompt("Number 2?");
+let rhouers = HOERS-num;
+let rmin = MIN-num2;
+ 
+console.log('Осталось: $ {rhours} часов и ${rmin}  минут'); 
+*/
+
+/*
+let num = +prompt("Number?");
+let dev = Math.trunc(num / 10);
+let last = num - del * 10;
+let res = last * 10000 + dev
+
+console.log(res);*/
+
+
+/*
+let number1 = +prompt("Number1= ") ;
+let dev = Math.trunc(number1 / 10);
+let ost = number1 % 10  ;
+console.log(`${ost}${dev}`);
+*/
+
+
+// const SOL = 250;
+
+// let total = +prompt("Number?");
+// let tsol = SOL + total * 0.1;
+
+// console.log(tsol);
+
+
+
+//урок 2 на уроке 
+
+// let num = +prompt('Введите число');
+
+//  if (typeof num != 'number') {
+
+//     if (num < 0) {
+//         console.log('Число отрицатеьное')
+//     } else if(num=0) {
+//         console.log('Число 0')
+//     } else if( num > 0 ) {
+//         console.log('Число положитеьное')
+//     } 
+//  } else{
+//      console.log(' Это не число!')
+//  }
+
+//  let num = prompt ('Введите возраст')
+//      if( num <0 || num > 120){
+//          console.log(' Это не корректное число')
+//      } else{
+//          console.log('  Урра коректный возраст)))')
+//}
+
+// let num = prompt (' Введите число')
+// let abs = Math.abs(num);
+
+// if (abs == NaN){
+//     console.log('Это не число!')
+// } else{
+//     console.log(abs)
+// }
+
+// let h = prompt('Введите часы')
+// let m = prompt('Введите минуты')
+// let s = prompt('Введите секунды')
+// if 
+
+
+// let month = + prompt (' Введите месяц');
+// switch ( month) {
+//     case 1:
+//         console.log ('Январь')
+//         break;
+//         case 2:
+//             console.log ('Февраль')
+//             break;
+//             case 3:
+//                 console.log ('Март')
+//                 break;
+//                 case 4:
+//                     console.log ('Апрель')
+//                     break;
+//                     case 5:
+//                         console.log ('Май')
+//                         break;
+//                         case 6:
+//                             console.log ('Июнь')
+//                             break;
+//                             case 7:
+//                                 console.log ('Июль')
+//                                 break;
+//                                 case 8:
+//                                     console.log ('Август')
+//                                     break;
+//                                     case 9:
+//                                         console.log ('Сентябрь')
+//                                         break;
+//                                         case 10:
+//                                             console.log ('Октябрь')
+//                                             break;
+//                                              case 11:
+//                                                   console.log ('Ноябрь')                
+//                                                     case 12:
+//                                                          console.log ('Декабрь')
+//                                                        break;
+//  default:
+//     console.log('Шо не понятно - от 1 до 12 сказано же!')                                          
+// }
+
+
+// let num1 = + prompt (' Введите число 1');
+// let num2 = + prompt (' Введите число 2');
+// let oper = + prompt (' Введите число 3');
+
+// switch (oper) {
+//     case '+':
+//         console.log('num1 + num2')
+//         break;
+//     case '-':
+//         console.log('num1 - num2')
+//         break;
+//         case '*':
+//             console.log('num1 * num2')
+//             break;
+//             case '/':
+//                 console.log('num1 / num2')
+//                 break;
+//     }
+
+
+// let num1 = + prompt (' Введите число 1');
+// let num2 = + prompt (' Введите число 2');
+
+// (num1 > num2)? console.log('nuun1') :console.log('num2') 
+
+
+
+// let num1 = + prompt (' Введите число 1');
+// (num !==0) ? console.log(' Кратно') : console.log(' Не кратно')   
+
+
+
+// let num1 = + prompt (' Введите название Планеты');
+// (num 'Земля' || 'земля') ?  console.log(' привет Землянин!') : console.log(' привет инопланетянин!')   
